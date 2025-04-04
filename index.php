397b0b54a3daffcfdcd23a873a5d1c0f1d51b6ec
yateen0 <150786316+yateen0@users.noreply.github.com>
2025-04-04 13:51:43 +0200

    begin kaarten class

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+//De class Game
+class Game{
+// Methode om het deck te initialiseren
+public function initializeDeck() 
+    // Kleuren en nummers
+    $colors = ['Rood', 'Geel', 'Groen', 'Blauw'];
+    $numbers = range(0, 9);
+    $specials = ['Skip', 'Reverse', 'Draw Two']; }
 
-class Card {
-    constructor(name, value) {
-        this.name = name;
-        this.value = value;
-    }
-}
